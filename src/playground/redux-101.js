56786f86fdb0e59dd5d156de6a8344f80c602141
madhusudan <madhusudhan638@gmail.com>
2019-11-30 13:28:03 +0530

    dispatch actions to change the state values

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,11 +1,53 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }) => {
+const store = createStore((state = { count: 0 }, action) => {
   // by doing state = we are assigning a default state
-  return state;
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 });
 
 console.log(store.getState());
 
+// actions - an object that gets sent to the store
+
+// increment, decrement, reset
+
 // i would like to increase the count by 1 or decrement by 1
 // we can achieve this using Actions
+
+// i would like to increment the count by 1
+store.dispatch({
+  type: "INCREMENT"
+});
+
+store.dispatch({
+  type: "INCREMENT"
+});
+
+store.dispatch({
+  type: "DECREMENT"
+});
+
+store.dispatch({
+  type: "RESET"
+});
+
+store.dispatch({
+  type: "DECREMENT"
+});
+
+console.log(store.getState());
