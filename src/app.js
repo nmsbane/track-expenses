a147e2560a609703c74ac4433da43e6f5c53918f
madhusudan <madhusudhan638@gmail.com>
2019-11-30 06:51:37 +0530

    seperate out route files into their own components

--- src/app.js
+++ src/app.js
@@ -1,57 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 // react differentiates between normal html and react component using uppercase letter
+import AppRouter from "./routers/AppRouter";
 
-const ExpenseDashboardPage = () => <div>This is from dashboard component</div>;
-
-const AddExpensePage = () => <div>This is from AddExpensePage component</div>;
-
-const EditExpensePage = () => <div>This is from EditExpensePage component</div>;
-
-const HelpPage = () => <div>This is from help page</div>;
-
-const NotFoundPage = () => (
-  <div>
-    404! - <Link to="/">Go home</Link>
-  </div>
-);
-
-// use NavLink inside of main navigation, to style the active url
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Create Expense
-    </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">
-      Edit expense
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
-  </header>
-);
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
