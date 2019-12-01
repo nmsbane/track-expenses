b4cd229cbe33185a50a51cc85313851413356de1
madhusudan <madhusudhan638@gmail.com>
2019-12-01 08:04:41 +0530

    use combineReducers to put two reducers into a single object

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,38 @@
 import { createStore, combineReducers } from "redux";
+
+// possible actions
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses reducer
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const filtersReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
+// Filter reducer
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 const demoState = {
   expenses: [
     {
@@ -16,3 +50,12 @@ const demoState = {
     endDate: undefined
   }
 };
+
+// create new store
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+console.log(store.getState());
