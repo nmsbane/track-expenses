58bfedebf6b53df0295c9be61f420f8f803adc9c
madhusudan <madhusudhan638@gmail.com>
2019-12-07 14:15:37 +0530

    implement controlled inputs for filters, now componenets can trigger actions using dispatch

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
 
-export default ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ description, amount, createdAt, id, dispatch }) => (
   <div>
     <h3>{description}</h3>
     <p>
       {amount} - {createdAt}
     </p>
+    <button
+      onClick={e => {
+        dispatch(removeExpense({ id }));
+      }}
+    >
+      Remove
+    </button>
   </div>
 );
+
+export default connect()(ExpenseListItem);
