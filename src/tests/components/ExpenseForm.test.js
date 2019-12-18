8672105c999ff523fc75c70bd45c13222262bffc
madhusudan <madhusudhan638@gmail.com>
2019-12-18 07:03:17 +0530

    complete writing test cases for ExpenseForm component, learnt about spies ( or mocks ), simuate events, also, how to mock standard library function using jest

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import ExpenseForm from "../../components/ExpenseForm";
 import expenses from "../fixtures/expenses";
+import moment from "moment";
 
 test("should render expense form correctly", () => {
   const wrapper = shallow(<ExpenseForm />);
@@ -58,4 +59,33 @@ test("should not set amount if invalid input ", () => {
     .at(1)
     .simulate("change", { target: { value } });
   expect(wrapper.state("amount")).toBe("");
+});
+
+test("should call onsubmit prop for valid form submission", () => {
+  const onSubmitSpy = jest.fn();
+  const wrapper = shallow(
+    <ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />
+  );
+  wrapper.find("form").simulate("submit", { preventDefault: () => {} });
+  expect(wrapper.state("error")).toBe("");
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    note: expenses[0].note,
+    amount: expenses[0].amount,
+    createdAt: expenses[0].createdAt
+  });
+});
+
+test("should set new date on date change", () => {
+  const now = moment();
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("SingleDatePicker").prop("onDateChange")(now);
+  expect(wrapper.state("createdAt")).toBe(now);
+});
+
+test("should set calendar focused value on calanderfocus change", () => {
+  const focused = true;
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("SingleDatePicker").prop("onFocusChange")({ focused });
+  expect(wrapper.state("calendarFocused")).toBe(focused);
 });
