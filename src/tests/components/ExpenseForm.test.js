7b1df9e4d2e9048a7067ceb31412bb3490bd03d9
madhusudan <madhusudhan638@gmail.com>
2019-12-18 06:35:53 +0530

    write test cases for simulating onchange, onsubmit using enzymes simulate

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,50 @@ test("should render expense form correctly with expense data", () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[0]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test("should render error for invalid form submission", () => {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("form").simulate("submit", { preventDefault: () => {} }); // in onSubmit code, we are using e.preventDefault
+  expect(wrapper.state("error").length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should set description on input change", () => {
+  const value = "new description";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find("input")
+    .at(0)
+    .simulate("change", { target: { value: value } });
+  expect(wrapper.state("description")).toBe(value);
+});
+
+// should set note ontext area change
+test("should set note ontext area change", () => {
+  const value = "new text area";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find("textarea").simulate("change", { target: { value } });
+  expect(wrapper.state("note")).toBe(value);
+});
+
+// should set amount if valid input ( 23. 50 )
+test("should set amount if valid input", () => {
+  const value = "23.50";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find("input")
+    .at(1)
+    .simulate("change", { target: { value } });
+  expect(wrapper.state("amount")).toBe(value);
+});
+
+// should not set amount if invalid input ( 12.122 )
+test("should not set amount if invalid input ", () => {
+  const value = "12.122";
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find("input")
+    .at(1)
+    .simulate("change", { target: { value } });
+  expect(wrapper.state("amount")).toBe("");
+});
