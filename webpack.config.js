35ba8fa8f746ad960ca4ec1080f6c12dcc7f37ef
madhusudan <madhusudhan638@gmail.com>
2019-12-21 17:08:37 +0530

    moving towards creating production buld using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,32 @@
 // webpack.config.js file is just a node script
 const path = require("path");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"), // absolute path
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"] // since using both loaders, use "use"
-      }
-    ]
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true
-  }
+module.exports = env => {
+  const isProduction = env === "production" ? true : false;
+
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"), // absolute path
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"] // since using both loaders, use "use"
+        }
+      ]
+    },
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true
+    }
+  };
 };
